deac78f4de9aefc4f9495958d73c1f8e806f81fe
Phuwadon <88771136+kappakap39@users.noreply.github.com>
2023-12-18 15:49:08 +0700

    up

--- src/controllers/authFileController.ts
+++ src/controllers/authFileController.ts
@@ -5,7 +5,7 @@ import prisma from '../lib/db';
 import { PrismaClient } from '@prisma/client';
 
 const storage = multer.diskStorage({
-    destination: '../../assets/uploads',
+    destination: './assets/uploads',
     filename: (req, file, cb) => {
         cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
     },
